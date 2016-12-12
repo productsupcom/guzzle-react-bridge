e0d778fc241db9b41c5fb6655d98518efd7da1d1
Alexey Shockov <alexey@shockov.com>
2016-12-12 23:39:09 +0100

    Better examples

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # guzzle-react-bridge
 
-Guzzle HTTP client integration with ReactPHP event loop
+Guzzle HTTP client integration with ReactPHP event loop.
+
+Please, take a look at [the examples](example/).
--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,17 @@ use Closure;
 use GuzzleHttp\Promise\Promise;
 use React\EventLoop\Factory;
 use React\EventLoop\LoopInterface;
+use function GuzzleHttp\Promise\coroutine;
 
+/**
+ * Run an action inside a fresh event loop
+ *
+ * Shortcut to create an event loop with default settings, that suits for most cases.
+ *
+ * @param callable $action
+ *
+ * @return void
+ */
 function run(callable $action)
 {
     $loop = Factory::create();
@@ -14,6 +24,34 @@ function run(callable $action)
     \GuzzleHttp\Promise\queue(new ReactTaskQueue($loop));
 
     $loop->nextTick($action);
+
+    $loop->run();
+}
+
+/**
+ * Run a coroutine inside a fresh event loop
+ *
+ * Shortcut to create an event loop with default settings, that suits for most cases.
+ *
+ * @param callable $coroutine
+ *
+ * @return void
+ */
+function run_coroutine_fn(callable $coroutine)
+{
+    $loop = Factory::create();
+
+    $coroutineFn = function () use ($loop, $coroutine) {
+        return $coroutine($loop);
+    };
+
+    \GuzzleHttp\Promise\queue(new ReactTaskQueue($loop));
+
+    $loop->nextTick(function () use ($coroutineFn) {
+        $coroutineInvocation = coroutine($coroutineFn);
+
+        // Here we are, waiting for the coroutine (promise) to complete.
+    });
 
     $loop->run();
 }
